03666d0e6b5f2ffe4455140363ba06d3a55bb344
hugues0 <ntwari.hugues@gmail.com>
2021-07-22 11:21:16 +0200

    added separate routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
-import propTypes from 'prop-types'
-import React from 'react'
-import Button from './Button'
+import propTypes from "prop-types";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
 
-const Header = ({title,onAdd,showAdd}) => {
-   
-    return (
-      <header className="header">
-        <h1>{title}</h1>
-        <Button text={showAdd ? "Hide" : "Add"} color={showAdd ? "red" : "green" } onClick={onAdd}/>
-        
-      </header>
-    );
-}
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === '/' && <Button
+        text={showAdd ? "Hide" : "Add"}
+        color={showAdd ? "red" : "green"}
+        onClick={onAdd}
+      />}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: "Task tracker"
-}
+  title: "Task tracker",
+};
 
 Header.propTypes = {
-    title: propTypes.string.isRequired
-}
+  title: propTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
